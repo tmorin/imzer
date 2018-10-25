3203ecd1144bb84440fbb74034aacd774a60b879
tmorin <thibault@morin.io>
2018-10-25 10:40:04 +0200

    feat: add web-ext support

---
+++ public/background.js
@@ -0,0 +1,5 @@
+browser.browserAction.onClicked.addListener(function () {
+    browser.tabs.create({
+        url: "index.html"
+    });
+});
--- public/index.js
+++ public/index.js
@@ -5,8 +5,11 @@
 
     var map = L.map('map', {
         center: [0, 0],
-        zoom: 3
+        zoom: 3,
+        zoomControl: false
     });
+
+    new L.Control.Zoom({ position: 'bottomright' }).addTo(map);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
 }());
